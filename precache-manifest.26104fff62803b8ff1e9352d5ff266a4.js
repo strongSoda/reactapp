2098a08ae845ff836f073d54ec224b5675a84853
strongSoda <strongsoda2@gmail.com>
2018-10-17 20:36:41 +0530

    Updates

---
+++ precache-manifest.26104fff62803b8ff1e9352d5ff266a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75024734ab9c32c58e65",
+    "url": "/reactapp/static/js/runtime~main.75024734.js"
+  },
+  {
+    "revision": "e5f63554ba2e40a22bd7",
+    "url": "/reactapp/static/js/main.e5f63554.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/reactapp/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "e5f63554ba2e40a22bd7",
+    "url": "/reactapp/static/css/main.780464ef.chunk.css"
+  },
+  {
+    "revision": "d5d72a6e36b14678403d16ba07564651",
+    "url": "/reactapp/index.html"
+  }
+];
